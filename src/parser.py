ff197632ffd6b4d2f217484378b95917b068fbd2
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2021-04-19 14:42:45 -0300

    Testes e alterações em parsing

--- src/parser.py
+++ src/parser.py
@@ -17,6 +17,75 @@ def read(path):
         sys.stderr.write("'Não foi possível ler o arquivo: " + path + '. O seguinte erro foi gerado: ' + excep)
         os._exit(1)
 
+#Necessário devido a permutação de lugar das colunas: Indenizações - Contribuição Previdenciária
+def employees_parser_2020(file_path):
+    data = read(file_path)
+    
+    #Ajustando dataframe
+    data = data.dropna()
+    clean_currency(data,4,17)
+    
+    #Parsing data
+    rows = data.to_numpy()
+    employees = {}
+    
+    for row in rows:
+        reg = str(row[0]) #Matrícula 
+        name = row[1].strip() #Nome
+        role = row[2].strip() #Cargo
+        workplace = row[3].strip() #Lotação
+        remuneration = row[4] #Remuneração do cargo efetivo
+        other_verbs =  row[5] #Outras Verbas Remuneratórias, Legais ou Judiciais   
+        trust_pos = row[6] #Função de Confiança ou Cargo em Comissão 
+        christmas_grati = row[7] #Gratificação Natalina
+        terco_ferias = row[8] #Férias (1/3 constitucional)
+        abono_permanencia = row[9] #Abono de Permanência 
+        temp_remu = row[10] #Outras Remunerações Retroativas/Temporárias
+        idemnity = row[11] #Indenizações
+        total = row[12] #Total de rendimentos brutos
+        prev_contrib = row[13] #Contribuição Previdenciária
+        imposto_renda = row[14] #Imposto de Renda
+        ceil_ret = row[15] #Retenção do Teto       
+        
+        employees[reg] = {
+            'reg': reg,
+            'name': name,
+            'role': role,
+            'type': 'membro',
+            'workplace': workplace,
+            'active': True,
+            "income":
+        {
+            'total': total,
+            'wage': remuneration + other_verbs,
+            'perks':{
+                'total': idemnity,
+            },
+            'other':
+            { 
+                'total': trust_pos + christmas_grati + terco_ferias + abono_permanencia + temp_remu,
+                'trust_position': trust_pos,
+                'eventual_benefits': temp_remu,
+                'others_total': christmas_grati + terco_ferias + abono_permanencia,
+                'others': {
+                    'Gratificação Natalina': christmas_grati,
+                    'Férias (1/3 constitucional)': terco_ferias,
+                    'Abono de permanência': abono_permanencia,
+                }
+            },
+
+        },
+        'discounts':
+        {
+            'total': round(prev_contrib + ceil_ret + imposto_renda, 2),
+            'prev_contribution': prev_contrib,
+            'ceil_retention': ceil_ret,
+            'income_tax': imposto_renda
+        }
+    }
+    
+    return employees
+    
 def employees_parser(file_path):
     data = read(file_path)
     
@@ -95,60 +164,60 @@ def employees_idemnity(file_path, employees):
     #Parsing data
     rows  = data.to_numpy()
     for row in rows:
-        reg = str(row[0]) #Matrícula
-        prm =  row[5] #0053-L PREM S/TRB
+        reg = str(int(row[0])) #Matrícula
+        prm =  row[4] #0053-L PREM S/TRB
         
-        transporte =  row[6] #0243-AUX TRANSP
-        housing_aid = row[7] #0261-AUX MORADIA
-        aux_refei = row[8] #0269-AUX REFEICAO
-        aux_ali = row[9] #0270-AUX ALIM
-        aux_saude = row[10] #0272-AUX SAUDE
+        transporte =  row[5] #0243-AUX TRANSP
+        housing_aid = row[6] #0261-AUX MORADIA
+        aux_refei = row[7] #0269-AUX REFEICAO
+        aux_ali = row[8] #0270-AUX ALIM
+        aux_saude = row[9] #0272-AUX SAUDE
         
-        abon  = row[11] #0098-ABON PER ATR
-        diff = row[12] #0201-DIF ENTRANC
-        adc_ser_ex = row[13] #0244-ADC SERV EXT
-        serv_ex_pĺt = row[14] #0251-SERV EXT PLT
-        ind_ferias = row[15] #0265-IND FERIAS
-        adc_at_esp = row[16] #0267-ADC AT ESPEC
-        grt_sub = row[17] #0271-GRT SUB FGMP
-        adc_exercicio = row[18] #0275-AD EXERCICIO
-        adc_pr = row[19]  #0279-ADIC PR/TEMP
-        atr_venci = row[20] #0400-VENCIM ATR
-        diff_entr = row[21] #0401-DIF ENTR ATR
-        quino  = row[22] #0403-QUINQ ATR
-        fer_at = row[23] #0408-AB FER PR AT
-        pens_ali = row[24] #0416-PENS AL ATR
-        indz_ass = row[25] #0420-INDZ ASS ATR
-        sal_atr = row[26] #0426-13 SAL ATR
-        indz_coo = row[27] #0429-INDZ COO ATR
-        ad_li = row[28] #0434-AD L16307 AT
-        sub_atr = row[29] #0435-SUBSIDIO ATR
-        aux_trp = row[30] #0443-AUX TRP ATR
-        ad_sev_ex = row[31] #0444-AD SV EX ATR
-        ad_sev_pl = row[32] #0451-SV EX PL ATR
-        ad_com = row[33] #0459-COM LIC ATR
-        adc_ferias = row[34] #0463-A FERIAS ATR
-        ad_at = row[35] #0467-AD AT ES ATR
-        aux_ref_atr = row[36] #0469-AUX REF ATR
-        aux_ali_atr = row[37] #0470-AUX ALIM ATR
-        gr_su = row[38] #0471-GR SU FG ATR
-        aux_saude_atr = row[39] #0472-AUX SAUDE AT
-        adc_exe = row[40] #0475-ADIC EXE ATR
-        adc_pr = row[41] #0479-AD PR/TP ATR
-        grt_fgmp = row[42] #0480-GRT FGMP ATR
-        in_cgsmp = row[43] #0483-IN CGSMP ATR
-        ind_ouv = row[44] #0486-IND OUV ATR
-        ats_tribu = row[45] #522 ATS.TRIBUTAV
-        ats_ntribut = row[46] #523 ATS.N.TRIBUT
-        pae_ntrb = row[47] #558 PAE NTRB ATR
-        pae_trib = row[48] #559 PAE TRIB ATR
-        van_exer = row[49] #563 VAN.EXER.ATU
-        van_exer_ant = row[50] #564 VAN.EXER.ANT
+        abon  = row[10] #0098-ABON PER ATR
+        diff = row[11] #0201-DIF ENTRANC
+        adc_ser_ex = row[12] #0244-ADC SERV EXT
+        serv_ex_pĺt = row[13] #0251-SERV EXT PLT
+        ind_ferias = row[14] #0265-IND FERIAS
+        adc_at_esp = row[15] #0267-ADC AT ESPEC
+        grt_sub = row[16] #0271-GRT SUB FGMP
+        adc_exercicio = row[17] #0275-AD EXERCICIO
+        adc_pr = row[18]  #0279-ADIC PR/TEMP
+        atr_venci = row[19] #0400-VENCIM ATR
+        diff_entr = row[20] #0401-DIF ENTR ATR
+        quino  = row[21] #0403-QUINQ ATR
+        fer_at = row[22] #0408-AB FER PR AT
+        pens_ali = row[23] #0416-PENS AL ATR
+        indz_ass = row[24] #0420-INDZ ASS ATR
+        sal_atr = row[25] #0426-13 SAL ATR
+        indz_coo = row[26] #0429-INDZ COO ATR
+        ad_li = row[27] #0434-AD L16307 AT
+        sub_atr = row[28] #0435-SUBSIDIO ATR
+        aux_trp = row[29] #0443-AUX TRP ATR
+        ad_sev_ex = row[30] #0444-AD SV EX ATR
+        ad_sev_pl = row[31] #0451-SV EX PL ATR
+        ad_com = row[32] #0459-COM LIC ATR
+        adc_ferias = row[33] #0463-A FERIAS ATR
+        ad_at = row[34] #0467-AD AT ES ATR
+        aux_ref_atr = row[35] #0469-AUX REF ATR
+        aux_ali_atr = row[36] #0470-AUX ALIM ATR
+        gr_su = row[37] #0471-GR SU FG ATR
+        aux_saude_atr = row[38] #0472-AUX SAUDE AT
+        adc_exe = row[39] #0475-ADIC EXE ATR
+        adc_pr = row[40] #0479-AD PR/TP ATR
+        grt_fgmp = row[41] #0480-GRT FGMP ATR
+        in_cgsmp = row[42] #0483-IN CGSMP ATR
+        ind_ouv = row[43] #0486-IND OUV ATR
+        ats_tribu = row[44] #522 ATS.TRIBUTAV
+        ats_ntribut = row[45] #523 ATS.N.TRIBUT
+        pae_ntrb = row[46] #558 PAE NTRB ATR
+        pae_trib = row[47] #559 PAE TRIB ATR
+        van_exer = row[48] #563 VAN.EXER.ATU
+        van_exer_ant = row[49] #564 VAN.EXER.ANT
         
         emp = employees[reg]
         
         emp['income']['perks'].update({
-            'total': round(aux_ali + aux_refei + aux_saude , transporte, housing_aid),
+            'total': round(aux_ali + aux_refei + aux_saude + transporte + housing_aid, 2),
             'food': aux_ali + aux_refei,
             'health': aux_saude,
             'transportation': transporte,
@@ -206,13 +275,18 @@ def employees_idemnity(file_path, employees):
                 + in_cgsmp + ind_ouv + ats_tribu + ats_ntribut + pae_ntrb + pae_trib + van_exer 
                 + van_exer_ant, 2)
         })
-        
+    
+    return employees
+    
 def parse(file_names, month, year):
     employees = {}
     
     for file_name in file_names:
         if 'vi' not in file_name:
-            employees.update(employees_parser(file_name))
+            if int(year) <= 2019:
+                employees.update(employees_parser(file_name))
+            else:
+                employees.update(employees_parser_2020(file_name))
         elif int(month) >= 7 and int(year) == 2019 :
             employees.update(employees_idemnity(file_name, employees))
         elif int(year) > 2019:
--- src/parser_test.py
+++ src/parser_test.py
@@ -48,5 +48,98 @@ class TestParser(unittest.TestCase):
         self.assertEqual(1, len(employees))
         self.assertDictEqual(employees[0], expected)
         
+    def test_membros_jan_2020(self):
+        self.maxDiff = None
+        
+        expected = {    
+            'reg': '1892770',
+            'name': 'ADEMILTON DAS VIRGENS CARVALHO LEITÃO',
+            'role': 'PROMOTOR 2. ENTRANCIA',
+            'type': 'membro',
+            'workplace': 'GAB 1 PROM JUSTICA CRIMINAL PAULISTA',
+            'active': True,
+            'income': {
+                'total': 33572.65,
+                'wage': 32004.65,
+                'perks': {
+                    'total': 1568.00,
+                    'food': 1068.00,
+                    'health': 500.00,
+                    'transportation': 0.00,
+                    'housing_aid': 0.00,
+                },
+                'other': {
+                    'total': 0.00,
+                    'trust_position': 0.00,
+                    'others_total': 0.00,
+                    'eventual_benefits': 0.00,
+                    'others': {
+                        'Gratificação Natalina': 0.00,
+                        'Férias (1/3 constitucional)': 0.00,
+                        'Abono de permanência': 0.00,
+                        '0053-L PREM S/TRB': 0.00,
+                        '0098-ABON PER ATR': 0.00,
+                        '0201-DIF ENTRANC': 0.00,
+                        '0244-ADC SERV EXT': 0.00,
+                        '0251-SERV EXT PLT': 0.00,
+                        '0265-IND FERIAS': 0.00,
+                        '0267-ADC AT ESPEC': 0.00,
+                        '0271-GRT SUB FGMP': 0.00,
+                        '0275-AD EXERCICIO': 0.00, 
+                        '0279-ADIC PR/TEMP': 0.00,
+                        '0400-VENCIM ATR': 0.00,
+                        '0401-DIF ENTR ATR': 0.00,
+                        '0403-QUINQ ATR': 0.00,
+                        '0408-AB FER PR AT': 0.00,
+                        '0416-PENS AL ATR': 0.00,
+                        '0420-INDZ ASS ATR' : 0.00,
+                        '0426-13 SAL ATR': 0.00,
+                        '0429-INDZ COO ATR': 0.00,
+                        '0434-AD L16307 AT': 0.00,
+                        '0435-SUBSIDIO ATR': 0.00,
+                        '0443-AUX TRP ATR': 0.00,
+                        '0444-AD SV EX ATR': 0.00,
+                        '0451-SV EX PL ATR': 0.00,
+                        '0459-COM LIC ATR': 0.00,
+                        '0463-A FERIAS ATR': 0.00,
+                        '0467-AD AT ES ATR': 0.00,
+                        '0469-AUX REF ATR': 0.00,
+                        '0470-AUX ALIM ATR': 0.00, 
+                        '0471-GR SU FG ATR': 0.00,
+                        "0472-AUX SAUDE AT": 0.00,
+                        '0475-ADIC EXE ATR': 0.00,
+                        '0479-AD PR/TP ATR': 0.00,
+                        '0480-GRT FGMP ATR': 0.00,
+                        '0483-IN CGSMP ATR': 0.00,
+                        '0486-IND OUV ATR': 0.00,
+                        '522 ATS.TRIBUTAV': 0.00,
+                        '523 ATS.N.TRIBUT': 0.00,
+                        '558 PAE NTRB ATR': 0.00,
+                        '559 PAE TRIB ATR': 0.00,
+                        '563 VAN.EXER.ATU': 0.00,
+                        '564 VAN.EXER.ANT': 0.00,
+                    },
+            },
+        },
+            'discounts': {
+                'total': 10907.96,
+                'prev_contribution': 4320.63,
+                'ceil_retention': 0.00,
+                'income_tax': 6587.33,
+            }
+        }
+        
+        files = ('./output_test/2020_01_remu.xls',
+                './output_test/2020_01_vi.xls')
+        employees = parser.parse(files, '01', '2020')
+        
+        #Verificações    
+        self.assertEqual(1, len(employees))
+        self.assertDictEqual(employees[0], expected)
+
+        
+        
+        
+                
 if __name__ == '__main__':
     unittest.main()
