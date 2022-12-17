b2400c2c6912608ab7badbc6fab559a9d326737c
Joellensilva <joellen.absilva@gmail.com>
2022-12-16 21:30:14 -0300

    testando

--- src/crawler.py
+++ src/crawler.py
@@ -71,7 +71,7 @@ def download_codes(year, month):
             url = folder_url.format(
                 vi_year_codes[int(year)], url_complements[key], year)
 
-        source_page = requests.get(url).text
+        source_page = requests.get(url, verify=False).text
         soup = BeautifulSoup(source_page, features='lxml')
 
         # Intera sob as tags de download que contém o download code
